05e167c398d01a90fd452323cfffbdd3d27cee9f
Gabe <115848472+g-nbo@users.noreply.github.com>
2024-06-12 19:11:06 -0400

    Finish assignment

---
+++ src/components/MovieDisplay.jsx
@@ -0,0 +1,16 @@
+// You can also destructure your props directly from the parameter list
+export default function MovieDisplay({ movie }) {
+    if(movie) {
+        return (
+            <>
+              <h1>{movie.Title}</h1>
+              <h2>{movie.Genre}</h2>
+              <img src={movie.Poster} alt={movie.Title} />
+              <h2>{movie.Year}</h2>
+            </>
+          );
+    } else {
+        return 'Select a movie'
+    }
+    
+  }
